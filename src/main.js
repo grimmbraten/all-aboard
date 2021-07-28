28f1f1444c2d83b69804eca6d7ef47d647d78dcb
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-28 14:50:58 +0200

    extend all aboard funtions with new functionallity

--- src/main.js
+++ src/main.js
@@ -1,35 +1,57 @@
-const sequence = async (tasks, ...arguments) => {
+const hasValidTasks = tasks => {
   if (!Array.isArray(tasks))
     throw new TypeError(
-      `expected parameter with type of array, received ${typeof tasks}`
-    );
-
-  for (const task of tasks) {
-    if (typeof task !== "function")
-      throw new TypeError(
-        `expected [AsyncFunction: name] or [Function (anonymous)], received ${typeof task}`
-      );
-     arguments ? await task(...arguments) : await task();
-  }
-};
-
-const parallel = (tasks, ...arguments) => {
-  let promises = [];
-
-  if (!Array.isArray(tasks))
-    throw new TypeError(
-      `expected parameter with type of array, received ${typeof tasks} instead`
+      `expected tasks to be type of array, but received ${typeof tasks}`
     );
 
   tasks.forEach(task => {
     if (typeof task !== "function")
       throw new TypeError(
-        `expected [AsyncFunction: name] or [Function (anonymous)], received ${typeof task}`
+        `expected tasks to be type of [AsyncFunction: name] or [Function (anonymous)], but received ${typeof task}`
       );
+  });
+}
+
+const hasValidArguments = arguments => {
+  if(!(Array.isArray(arguments) || arguments === undefined))
+    throw new TypeError(
+      `expected arguments to be type of array or undefined, but received ${typeof arguments}`
+    );
+}
+
+const sequence = async (tasks, arguments, eject = false) => {
+  let proceed = true;
+  const settled = [];
+
+  hasValidTasks(tasks);
+  hasValidArguments(arguments);
+  
+  for (const task of tasks) {
+    if(proceed) {
+      try {
+        const response = arguments ? await task(...arguments) : await task();
+        settled.push({ status: 'fulfilled', value: response });
+      } catch (error) {
+        if (eject) proceed = false;
+        settled.push({ status: 'rejected', reason: error });
+      }
+    }
+  }
+
+  return settled;
+};
+
+const parallel = (tasks, arguments) => {
+  let promises = [];
+
+  hasValidTasks(tasks);
+  hasValidArguments(arguments);
+
+  tasks.forEach(task => {
     promises.push(arguments ? task(...arguments) : task());
   });
 
-  return Promise.all(promises);
+  return Promise.allSettled(promises);
 };
 
 module.exports = {
